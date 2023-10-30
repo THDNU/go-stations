cba691559309b9b3b91948e1e44008ea6207b285
THDNU <nakagawa.19rd@gmail.com>
2023-10-31 02:06:38 +0900

    Go-08

--- service/todo.go
+++ service/todo.go
@@ -23,7 +23,7 @@ func NewTODOService(db *sql.DB) *TODOService {
 func (s *TODOService) CreateTODO(ctx context.Context, subject, description string) (*model.TODO, error) {
 	const (
 		insert  = `INSERT INTO todos(subject, description) VALUES(?, ?)`
-		confirm = `SELECT subject, description, created_at, updated_at FROM todos WHERE id = ?`
+		confirm = `SELECT id, subject, description, created_at, updated_at FROM todos WHERE id = ?`
 	)
 
 	result, err := s.db.ExecContext(ctx, insert, subject, description)
